8e6737a41d0a91b5025b6d4832c0860c05b76dac
wiserd <wiserd@gmail.com>
2019-09-07 12:32:34 -0700

    percent discount bubble will vanish when discount is zero or less. recalculates on change

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-// import {ProductBundles} from "./modules/ProductBundles";
+//import {ProductBundles} from "./modules/ProductBundles";
 import './styles/main.scss';
 import loadSVGs from './modules/svg-replace';
+import hideZeroDiscount from './modules/hideZeroDiscount';
 import 'popper.js';
 import 'bootstrap';
 
 document.addEventListener('DOMContentLoaded', () => {
   loadSVGs();
+  hideZeroDiscount();
 });
